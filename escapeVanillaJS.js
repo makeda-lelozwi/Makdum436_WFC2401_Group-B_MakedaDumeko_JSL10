5f5842b5a257116cbe65d76287673799eca72977
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-04-18 22:58:40 +0200

    completed room three

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -30,11 +30,15 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("directions.json")
       .then((response) => response.json())
       .then((directions) => {
-        navigateLabyrinth(directions).then((message) => {
-          // 🪲 Bug: Incorrect method
-          document.getElementById("room3Result").textContent = message;
-        });
-      });
+        navigateLabyrinth(directions)
+          .then((message) => {
+            document.getElementById("room3Result").textContent = message;
+          })
+          .catch((error) =>
+            console.error("Error navigating the labyrinth ☹️", error)
+          );
+      })
+      .catch((error) => console.error("Error fetching directions 😵", error));
   });
 });
 
@@ -63,7 +67,7 @@ function findIntersection(setA, setB) {
 async function navigateLabyrinth(directions) {
   for (let direction of directions) {
     // 🪲 Bug: No delay
-    new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     console.log(`Navigating: ${direction.step}`);
   }
   return "Congratulations! You've mastered the essentials of Vanilla JavaScript. Welcome to the world of React, where you'll build powerful and dynamic web applications. Let's dive in!";
